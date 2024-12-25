8dbc3d9f6004daaef6733f4435218dd93e7ad0b7
xmostafalotfy <mostafamohammmed82@gmail.com>
2024-12-25 19:08:51 +0200

    Streams

--- Java/lab5/BetterSrting/Better.java
+++ Java/lab5/BetterSrting/Better.java
@@ -7,12 +7,16 @@ class Better {
     public static String betterString(String s1, String s2, TwoStringC predicate) {
         return predicate.test(s1, s2) ? s1 : s2;
     }
-
+    public static boolean aa(String s1, String s2) {
+        return true;
+    }
     public static void main(String[] args) {
         String string1 = "better";
         String string2 = "aaaaaaaahhhhhhhhhh";
 
-        String result = betterString(string1, string2, (s1, s2) -> s1.equals("better"));
+        String result = betterString(string1, string2, (s1, s2) ->true);
         System.out.println("Better String: " + result);
+        String result1= betterString(string1, string2, Better::aa);
+        System.out.println("Better String: " + result1);
     }
 }
