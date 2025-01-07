ba3f3a10f05b79cfd518ab26f9f9ebeaf8d3d021
xmostafalotfy <mostafamohammmed82@gmail.com>
2025-01-07 12:38:42 +0200

    lab2

---
+++ JavaScript/day2/lab2.js
@@ -0,0 +1,59 @@
+//  Digits Sum
+
+function digitSum(num){
+    if(num < 0 || isNaN(num)){throw Error("Only postive integers")}
+    var result =0;
+    while(num>0){
+        console.log(result)
+        result += num%10
+        num = Math.floor( num/ 10)
+    }
+    return result
+}
+
+// ------------------------------------------------------------------------------------------
+    // Vowels counter 
+
+    function vowelNums(str){
+        if (typeof str != 'string'){throw Error("Only Strings Available")}
+        var vowels = {a:0,e:0,o:0,u:0,i:0}
+        for(i = 0; i< str.length; i++){
+            if (Object.keys(vowels).includes(str.charAt(i))){vowels[str.charAt(i)]+=1}
+        }
+        return vowels
+    }
+
+    //                  OR                  //
+
+    function vowelNums(str){
+        if (typeof str != 'string'){throw Error("Only Strings Available")}
+        str = str.split("")
+        return str.reduce(function(vow,st){
+            if(Object.keys(vow).includes(st)){vow[st]+=1}
+            return vow
+        },{a:0,e:0,o:0,u:0,i:0})
+    }
+
+// ------------------------------------------------------------------------------------------------
+// Number of Occurence
+
+function numOfOccu(str,char){
+    if (typeof str != 'string'){throw Error("Only Strings Available")}
+    if (typeof char != 'string' || char.length !== 1){throw Error("Enter a valid Char")}
+    var charAppeareance = 0
+    for(i = 0; i< str.length; i++){
+        if (str.charAt(i) === char){charAppeareance += 1}
+    }
+    return charAppeareance
+}
+
+//------------------------------------------------------------------------------------------------
+// Bouns Question
+
+function calcAge(date){
+    date = new Date(date)
+    today = new Date()
+    if( date == 'Invalid Date'){throw Error("Invalid Date")}
+    if( date > today){throw Error("you're born in the future ????")}
+    return Math.floor((today - date)/(24*60*60*1000*365))
+}
